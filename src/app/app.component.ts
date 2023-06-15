18acab2c1b6747b428d56e3469c521419e34fd2f
PavanGujjarlapudi <62017544+PavanGujjarlapudi@users.noreply.github.com>
2023-06-15 10:12:15 -0500

    upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todoApp';
+  public isLogin = false;
+
+  onClick(){
+      this.isLogin = !this.isLogin;
+      console.log();
+  }
 }
